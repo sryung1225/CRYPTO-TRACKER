3d88ee909f547d786b90f3550d27405ef4ec183d
sryung <sryung1225@gmail.com>
2023-09-08 00:07:50 +0900

    [🥁🎀] 차트 탭 구현하기 - 차트를 표현하기 위해 라이브러리 APEXCHART 사용

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,11 +1,65 @@
 import { useQuery } from "react-query";
 import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
+import { theme } from "../theme";
 
 function Chart({ coinId }: IChartProps) {
   const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () =>
     fetchCoinHistory(coinId)
   );
-  return <h1>Chart</h1>;
+  return (
+    <div>
+      {isLoading ? (
+        "Loading chart..."
+      ) : (
+        <ApexChart
+          type="line"
+          series={[
+            {
+              name: "Price",
+              data: data?.map((price) => price.close) as number[],
+            },
+          ]}
+          options={{
+            theme: {
+              mode: "dark",
+            },
+            chart: {
+              height: 300,
+              width: 500,
+              toolbar: { show: false },
+              background: "transparent",
+            },
+            colors: [theme.accentColor],
+            fill: {
+              type: "gradient",
+              gradient: { gradientToColors: ["white"], stops: [0, 100] },
+            },
+            stroke: {
+              curve: "smooth",
+              width: 4,
+            },
+            grid: { show: false },
+            xaxis: {
+              axisBorder: { show: false },
+              axisTicks: { show: false },
+              labels: { show: true, rotate: 0 },
+              type: "datetime",
+              categories: data?.map((price) =>
+                new Date(Number(price.time_close) * 1000).toUTCString()
+              ),
+            },
+            yaxis: { show: false },
+            tooltip: {
+              y: {
+                formatter: (value) => `$${value.toFixed(4)}`,
+              },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 }
 
 interface IHistorical {
