6043c443009268d9f4b3adf438547a5ec56593f0
sryung <sryung1225@gmail.com>
2023-09-07 22:54:30 +0900

    [🥁] 차트 탭 데이터 연결하기 - api로 연결한 데이터 ohlcv에 대한 인터페이스 적용

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -2,10 +2,21 @@ import { useQuery } from "react-query";
 import { fetchCoinHistory } from "../api";
 
 function Chart({ coinId }: IChartProps) {
-  const { isLoading, data } = useQuery(["ohlcv", coinId], () =>
+  const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () =>
     fetchCoinHistory(coinId)
   );
   return <h1>Chart</h1>;
+}
+
+interface IHistorical {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
 }
 
 interface IChartProps {
