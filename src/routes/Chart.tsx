ed497bded57dad8629be4f0ec6642c4a61d4748f
sryung <sryung1225@gmail.com>
2023-09-07 16:46:36 +0900

    [🥁] 차트 탭 데이터 연결하기

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,5 +1,15 @@
-function Chart() {
+import { useQuery } from "react-query";
+import { fetchCoinHistory } from "../api";
+
+function Chart({ coinId }: IChartProps) {
+  const { isLoading, data } = useQuery(["ohlcv", coinId], () =>
+    fetchCoinHistory(coinId)
+  );
   return <h1>Chart</h1>;
+}
+
+interface IChartProps {
+  coinId: string;
 }
 
 export default Chart;
