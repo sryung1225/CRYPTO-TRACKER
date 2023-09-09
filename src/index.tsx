80f0f0e81ccecd379928240a654219d4660eb376
sryung <sryung1225@gmail.com>
2023-09-09 23:40:20 +0900

    [⚙] Recoil 설치

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { RecoilRoot } from "recoil";
 import App from "./App";
 
 const queryClient = new QueryClient();
@@ -10,8 +11,10 @@ const root = ReactDOM.createRoot(
 
 root.render(
   // <React.StrictMode>
-  <QueryClientProvider client={queryClient}>
-    <App />
-  </QueryClientProvider>
+  <RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </RecoilRoot>
   // </React.StrictMode>
 );
