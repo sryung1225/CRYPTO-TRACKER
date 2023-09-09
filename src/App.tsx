dd5794e1845936c496f9237832f83a930b9beeb6
sryung <sryung1225@gmail.com>
2023-09-10 04:12:34 +0900

    [🎀] 리셋 스타일링 수정

--- src/App.tsx
+++ src/App.tsx
@@ -32,34 +32,13 @@ article, aside, details, figcaption, figure,
 footer, header, hgroup, main, menu, nav, section {
   display: block;
 }
-/* HTML5 hidden-attribute fix for newer browsers */
-*[hidden] {
-    display: none;
-}
-body {
-  line-height: 1;
-}
-menu, ol, ul {
-  list-style: none;
-}
-blockquote, q {
-  quotes: none;
-}
-blockquote:before, blockquote:after,
-q:before, q:after {
-  content: '';
-  content: none;
-}
-table {
-  border-collapse: collapse;
-  border-spacing: 0;
-}
 * {
   box-sizing: border-box;
 }
 body {
   font-family: 'Source Sans Pro', sans-serif;
   font-weight: 300;
+  line-height: 1;
   background-color: ${(props) => props.theme.bgColor};
   color: ${(props) => props.theme.textColor};
   line-height: 1.2;
@@ -67,6 +46,9 @@ body {
 a {
   color: inherit;
   text-decoration:none;
+}
+button {
+  cursor: pointer;
 }
 `;
 
